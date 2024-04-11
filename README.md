131ba298bb0baa765a85f8065ca828d01d8d4f8a
Sanches-23 <pasechnik23.2002@gmail.com>
2024-04-11 14:56:45 +0300

    Commit message

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # Creative Bakery landing page
 
-Implement landing page according to [Figma design](https://www.figma.com/file/dY3izAm0Vspsmra4lQWQIP/Bakerlab-FE-students?node-id=0%3A1) - Use BEM and SCSS
-
-- The design 1440px
-- Desktop 1280px
-- Tablet 640px
-- Mobile (> 320px)
-
-
 ## Github flow
 1. **Fork** the repo.
 2. **Clone** the forked one. (The project link should have your name but not `mate-academy`)
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+  const checkbox = document.getElementById('menuCheckbox');
+  const headerAside = document.querySelector('.header__aside');
+
+  checkbox.addEventListener('change', function () {
+    if (this.checked) {
+      headerAside.style.transform = 'translateX(0)';
+      headerAside.style.display = 'block';
+    } else {
+      headerAside.style.transform = '';
+      headerAside.style.display = 'none';
+    }
+  });
+});
