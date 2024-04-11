ff97dfdcadcdd96cbe975aa3ae791181647911bf
Sanches-23 <pasechnik23.2002@gmail.com>
2024-04-11 15:34:22 +0300

    make build

---
+++ index.a4cf82e4.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("menuCheckbox"),t=document.querySelector(".header__aside");e.addEventListener("change",function(){this.checked?(t.style.transform="translateX(0)",t.style.display="block"):(t.style.transform="",t.style.display="none")})});
+//# sourceMappingURL=index.a4cf82e4.js.map
